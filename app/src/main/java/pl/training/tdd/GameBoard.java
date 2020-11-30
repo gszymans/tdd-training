378cfebb50725797d3fc80ebf2b20d56e153caf5
Grzegorz Szymański <gszymans@gmail.com>
2020-11-30 15:43:40 +0100

    Init new game board with determined size (red)

--- app/src/main/java/pl/training/tdd/GameBoard.java
+++ app/src/main/java/pl/training/tdd/GameBoard.java
@@ -1,4 +1,11 @@
 package pl.training.tdd;
 
 class GameBoard {
+
+    public GameBoard(Integer verticalSize, Integer horizontalSize) {
+    }
+
+    public Integer getVerticalSize() {
+        return null;
+    }
 }
--- app/src/test/java/pl/training/tdd/BoardTest.java
+++ app/src/test/java/pl/training/tdd/BoardTest.java
@@ -6,13 +6,21 @@ import org.junit.jupiter.api.Test;
 
 class BoardTest {
 
+
+
     @DisplayName("Init new game board, expecting non null object")
     @Test
     void initGameBoardTest(){
-        var gamebord = new GameBoard();
+        var gamebord = new GameBoard(3,3);
         Assertions.assertNotNull(gamebord);
     }
 
+    @DisplayName("Init new game board with determined size")
+    @Test
+    void initGameBoardWithSpecifiedSize(){
+        var gamebord = new GameBoard(3,3);
+        Assertions.assertEquals(gamebord.getVerticalSize(), 3 );
+    }
 
 
 }
