1244d11e5a39649621496c553ced18dd622b1900
Grzegorz Szymański <gszymans@gmail.com>
2020-12-01 12:30:08 +0100

    Given movement can occupied the field (red)

---
+++ app/src/test/java/pl/training/tdd/ApplicationTest.java
@@ -0,0 +1,11 @@
+package pl.training.tdd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+
+
+}
