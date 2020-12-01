e20e1a69cb64a332f6bae0c888ac5cb1f38794cf
Grzegorz Szymański <gszymans@gmail.com>
2020-12-01 12:50:15 +0100

    check_if_money_currency_is_usd (green)

--- app/src/main/java/pl/training/tdd/Money.java
+++ app/src/main/java/pl/training/tdd/Money.java
@@ -1,7 +1,14 @@
 package pl.training.tdd;
 
 public class Money{
+
+    private final CurrencyType currency;
+
+    public Money(CurrencyType currency) {
+        this.currency = currency;
+    }
+
     public CurrencyType getCurrency() {
-        return null;
+        return currency;
     }
 }
--- app/src/test/java/pl/training/tdd/MoneyTest.java
+++ app/src/test/java/pl/training/tdd/MoneyTest.java
@@ -5,10 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
 
-
-
     @Test
-    void check_if_money_currency_is_usd(){
-        Assertions.assertEquals(CurrencyType.USD, (new Money()).getCurrency());
+    void check_if_money_currency_is_usd() {
+        Assertions.assertEquals(CurrencyType.USD, (new Money(CurrencyType.USD)).getCurrency());
     }
 }
